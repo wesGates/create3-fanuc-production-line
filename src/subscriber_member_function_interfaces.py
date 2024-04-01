aa61e4cdce122b3b3d84a9e9947e20f460fbfb5c
wesGates <wkgatesde@gmail.com>
2024-04-01 13:01:56 -0700

    Init Commit

---
+++ src/subscriber_member_function_interfaces.py
@@ -0,0 +1,42 @@
+import rclpy
+from rclpy.node import Node
+
+import sys
+sys.path.append("../dependencies/")
+print("PRINT PATH: ", sys.path)
+
+# This is important for running your nodes from the terminal
+from pynput.keyboard import KeyCode
+
+import my_interfaces
+from my_interfaces.msg import Num   # CHANGE
+
+
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            Num,                                              # CHANGE
+            'topic',
+            self.listener_callback,
+            10)
+        self.subscription
+
+    def listener_callback(self, msg):
+            self.get_logger().info('I heard: "%d"' % msg.num) # CHANGE
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_subscriber = MinimalSubscriber()
+
+    rclpy.spin(minimal_subscriber)
+
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
