b3442f782933327667d7df0a3ffe73a90d6b0e0a
wesGates <wkgatesde@gmail.com>
2024-04-03 14:28:44 -0700

    Made a dummy roomba_node that can request changes to the robot_ready_status topic

--- src/publisher_member_function_interfaces.py
+++ src/publisher_member_function_interfaces.py
@@ -1,9 +1,12 @@
-import rclpy
-from rclpy.node import Node
+""" 
+This is the big publisher that publishes all ready statuses
+"""
 
 import sys
 sys.path.append("../dependencies/")
-# DEBUG: print("PRINT PATH: ", sys.path)
+
+import rclpy
+from rclpy.node import Node
 
 # This is important for running your nodes from the terminal
 from pynput.keyboard import KeyCode
@@ -11,16 +14,17 @@ from pynput.keyboard import KeyCode
 import my_interfaces
 from my_interfaces.msg import Num, ReadyStatus, Base3status, Base2status   # CHANGE depending on messages to include
 
-# This is the big publisher that publishes the status of all ready statuses
-
 # class Base2Publisher(Node):
-class ReadyStatusPublisher(Node):
+class ReadyStatusPublisherNode(Node):
 
+    # def __init__(self, namespace):
     def __init__(self):
+        
         super().__init__('ready_status_publisher')
         self.ready_status_publisher_ = self.create_publisher(ReadyStatus, 'robot_ready_status', 10)     # CHANGE
-        timer_period = 1.0
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        
+        # timer_period = 1.0
+        # self.timer = self.create_timer(timer_period, self.timer_callback)
 
         # Initialize variables
         self.roomba = False
@@ -28,48 +32,87 @@ class ReadyStatusPublisher(Node):
         self.bunsen = False
 
 
-    def timer_callback(self):
-        # Continuously read the ready states
-        msg = ReadyStatus() # CHANGE
+    # def timer_callback(self):
 
+    #     print("Start of the publisher's timer_callback")
+    #     # Continuously read the ready states
+    #     msg = ReadyStatus() # msg is set to the ReadyStatus msg
+
+    #     # Ex) how to access the ready state for each value
+    #     msg.roomba = self.roomba
+    #     msg.beaker = self.beaker
+    #     msg.bunsen = self.bunsen
+
+    #     # # DEBUGGING
+    #     print("roomba status (within the publisher): ", msg.roomba)
+    #     print("beaker status (within the publisher): ", msg.beaker)
+    #     print("beaker status (within the publisher): ", msg.bunsen)
+
+    #     # ## TESTING: Change the message to be published
+    #     # msg.roomba = True
+    #     # print("Roomba message after changing to true, within publisher:", msg.roomba)
+
+    #     # Publish the message
+    #     print("Full message published by the publisher:", msg)
+    #     self.ready_status_publisher_.publish(msg)
+
+    #     # self.get_logger().info('Publishing roomba status: "%d"' % msg.roomba)  # CHANGE
+    #     # self.get_logger().info('Publishing beaker status: "%d"' % msg.beaker)  # CHANGE
+    #     # self.get_logger().info('Publishing beaker status: "%d"' % msg.bunsen)  # CHANGE
+    #     print("End of the publisher's timer_callback")
+
+
+    def set_ready_status(self, roomba_status=None, beaker_status=None, bunsen_status=None):
+        """
+        Update the internal status of roomba, beaker, and bunsen. Only updates values that are provided.
+
+        :param roomba_status: A boolean value indicating Roomba's ready status or None if no update is needed.
+        :param beaker_status: A boolean value indicating Beaker's ready status or None if no update is needed.
+        :param bunsen_status: A boolean value indicating Bunsen's ready status or None if no update is needed.
+        """
+        print(f"Statuses before set_ready_status () update - Roomba: {self.roomba}, Beaker: {self.beaker}, Bunsen: {self.bunsen}")
+
+        if roomba_status is not None:
+            self.roomba = roomba_status
+        if beaker_status is not None:
+            self.beaker = beaker_status
+        if bunsen_status is not None:
+            self.bunsen = bunsen_status
+
+        print(f"Updated statuses - Roomba: {self.roomba}, Beaker: {self.beaker}, Bunsen: {self.bunsen}")
+
+
+    def publish_ready_status(self):
+        """
+        Publish the status of robots.
+
+        :param ready: A boolean value indicating if Roomba is ready/has reached the position.
+        """
+        print("Start of the publish_ready_status() function within the publisher node")
+        # msg = Bool()
+        msg = ReadyStatus()
         msg.roomba = self.roomba
         msg.beaker = self.beaker
         msg.bunsen = self.bunsen
 
-        # # DEBUG
-        print("roomba status: ", msg.roomba)
-        print("beaker status: ", msg.beaker)
-        print("beaker status: ", msg.bunsen)
-
-        # Publish the message
         self.ready_status_publisher_.publish(msg)
-        print("\n")
+        print("Ready status published using publish_ready_status() ", msg)
+        print("End of the publish_ready_status() function within the publisher node")
 
-        # self.get_logger().info('Publishing roomba status: "%d"' % msg.roomba)  # CHANGE
-        # self.get_logger().info('Publishing beaker status: "%d"' % msg.beaker)  # CHANGE
-        # self.get_logger().info('Publishing beaker status: "%d"' % msg.bunsen)  # CHANGE
-
-
-    def roomba_status_updater(self, ready):
-        """
-        Update the status of roomba.
-
-        :param ready: A boolean value indicating if Roomba is ready/has reached the position.
-        """
-        self.roomba = ready
-        print(f"Updated Roomba status to: {self.roomba}")
+        # print(f"Updated Roomba status to: {self.roomba}")
 
 
-def main(args=None):
-    rclpy.init(args=args)
+# def main(args=None):
+#     rclpy.init(args=args)
 
-    ready_status_publisher = ReadyStatusPublisher()
+#     ready_status_publisher = ReadyStatusPublisher()
+#     rclpy.spin(ready_status_publisher)
 
-    rclpy.spin(ready_status_publisher)
+#     ready_status_publisher.roomba_status_updater(read=True)
 
-    ready_status_publisher.destroy_node()
-    rclpy.shutdown()
+#     ready_status_publisher.destroy_node()
+#     rclpy.shutdown()
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
--- src/roomba_node.py
+++ src/roomba_node.py
@@ -13,45 +13,44 @@ from key_commander import KeyCommander
 
 import my_interfaces
 from my_interfaces.msg import ReadyStatus, Num, Base2status, Base3status   # CHANGE
-from publisher_member_function_interfaces import ReadyStatusPublisher
+from publisher_member_function_interfaces import ReadyStatusPublisherNode
 
 # Initialize and connect to OTHER nodes
 rclpy.init()
 namespace = 'create3_05AE'
-ready_status_publisher = ReadyStatusPublisher()
-# ready_status_subscriber = ReadyStatusSubscriber()
+ready_status_publisher_node = ReadyStatusPublisherNode()
 
-class ReadyStatusSubscriber(Node):
+class RoombaNode(Node):
 
 	# def __init__(self, namespace):
 	def __init__(self):
-		super().__init__('ready_status_subscriber')
+		# super().__init__('ready_status_subscriber')
+		super().__init__('roomba_node')
+
 
 		# Establish the node objects within the class for node operations
-		self.ready_status_publisher = ready_status_publisher
+		self.ready_status = ready_status_publisher_node
 
 		self.ready_status_subscription_ = self.create_subscription(ReadyStatus, 'robot_ready_status', 
 															 self.ready_status_callback, 10)
 
-		# self.ready_status_subscription_ = self.create_subscription(ReadyStatus, 'robot_ready_status', 
-		# 													 self.listener_callback, 10)
-		
 		# Initializing variables to store data received from other nodes
         # These variables are updated in the callback functions.
 		self.latest_ready_status = None
 
 	def ready_status_callback(self, msg):
 		# Update the latest ready statuses with the message received from the 'robot_ready_status' topic
+		print("I heard...")
 		self.latest_ready_status = msg
 		self.roomba_status = self.latest_ready_status.roomba
 
 		# DEBUGGING
-		print("Printing msg.roomba: ", msg.roomba)
-		print("Printing msg.beaker: ", msg.beaker)
-		print("Printing msg.bunsen: ", msg.bunsen)
+		print("msg.roomba (received from publisher): ", msg.roomba)
+		print("msg.beaker (received from publisher): ", msg.beaker)
+		print("msg.bunsen (received from publisher): ", msg.bunsen)
 
-
-		self.get_logger().info(f"Received /robot_ready_status: {self.latest_ready_status}")
+		# self.get_logger().info(f"Received /robot_ready_status: {self.latest_ready_status}")
+		print("End of RoombaNode callback \n")
 
 
 	def listener_callback(self, msg):
@@ -60,24 +59,23 @@ class ReadyStatusSubscriber(Node):
 			self.get_logger().info('I heard beaker status: "%d"' % msg.bunsen) # CHANGE
 			print("\n")
 
-	def change_roomba_status(self, ready=False):
-		# Logic to determine if Roomba is ready (this is just a placeholder for actual logic)
-		# ready = not ready
-		ready = True
-		print("Ready status (roomba node): ", ready)
-		is_ready = ready  # or some condition that evaluates Roomba's readiness
-		self.ready_status_publisher.roomba_status_updater(is_ready)
-
 
 	def main(self):
 		try:
-			print("Step 1")
+			print("\nStep 1: Get the publisher to publish on command and access the information. ")
+			self.ready_status.publish_ready_status()
+			print("\n")
 			time.sleep(1.0)
-			ready_status_publisher.roomba_status_updater(ready=True)
-			self.change_roomba_status()
-			print("Step 2")
+
+
+			print("Step 2: Change the status of a variable")
+			self.ready_status.set_ready_status(roomba_status=True)
+			print("\n")
 			time.sleep(1.0)
-			print("Step 3")
+			
+			print("Step 3: Re-publish the hopefully altered statuses")
+			self.ready_status.publish_ready_status()
+			print("\n")
 		except Exception as error:
 			self.get_logger().error(f"Error in main: {error}") # Error logging
 
@@ -86,23 +84,18 @@ class ReadyStatusSubscriber(Node):
 if __name__ == '__main__':
 	# rclpy.init()
 
-
-	ready_status_publisher = ReadyStatusPublisher()
-
-
-
-
-
-
-	ready_status_subscriber = ReadyStatusSubscriber()
+	roomba = RoombaNode()
 	exec = MultiThreadedExecutor(8)
-	exec.add_node(ready_status_subscriber)
 
+	exec.add_node(roomba)
+	exec.add_node(ready_status_publisher_node)
 
+	time.sleep(1.0)
+	
 	keycom = KeyCommander([
-		(KeyCode(char='u'), ready_status_subscriber.main),
+		(KeyCode(char='u'), roomba.main),
 	])
-	print(" Press 'U' to intitiate launch")
+	print(" Press 'U' to intitiate routine")
 
 	try:
 		exec.spin()  # execute Roomba callbacks until shutdown or destroy is called
@@ -112,5 +105,5 @@ if __name__ == '__main__':
 		print(f"Unexpected error: {error}")
 	finally:
 		exec.shutdown()
-		ready_status_subscriber.destroy_node()
+		roomba.destroy_node()
 		rclpy.try_shutdown()
